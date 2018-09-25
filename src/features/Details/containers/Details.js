f81f643388b962c06b39d3ad1167be5ce094c3c8
Andrey Nebogin <anebogin@gmail.com>
2018-09-25 22:07:31 +0300

    Details page is in process

--- src/features/Details/containers/Details.js
+++ src/features/Details/containers/Details.js
@@ -1,20 +1,59 @@
 import React, {PureComponent} from 'react';
 import {
-    View, Text, Image, Button, TouchableOpacity, StatusBar
+    View, Text, Image, Button, TouchableOpacity, StatusBar, Linking, Alert
 } from 'react-native';
 import {connect} from 'react-redux';
 
 import styles from './Details.styles';
 
 class Details extends PureComponent {
+    handleVisit = () => {
+        const {nav} = this.props;
+        const {params: {item}} = nav.routes[nav.index];
+        const url = item.image.contextLink;
+        Linking.canOpenURL(url).then((supported) => {
+            if (supported) {
+                Linking.openURL(url);
+            } else {
+                Alert(`Don't know how to open URI: ${url}`);
+            }
+        });
+    }
+
+    handleOnBackPress = () => {
+        const {navigation} = this.props;
+        navigation.goBack();
+    }
+
     render() {
         const {nav} = this.props;
         const {params: {item}} = nav.routes[nav.index];
-        console.log(item);
         return (
             <View style={styles.container}>
                 <StatusBar backgroundColor="#fafafa" barStyle="dark-content" />
-                <Text>1111</Text>
+                <Image
+                    source={{uri: item.link}}
+                    resizeMode="contain"
+                    style={styles.image}
+                />
+                <View style={styles.content}>
+                    <Text
+                        style={styles.text}
+                        numberOfLines={2}
+                    >
+                        {item.title}
+                    </Text>
+                    <Button
+                        onPress={this.handleVisit}
+                        title="Visit"
+                    />
+                </View>
+                <TouchableOpacity
+                    onPress={this.handleOnBackPress}
+                    style={styles.backButton}
+                >
+                    <Text style={styles.backButtonText}>{'< Back'}</Text>
+                </TouchableOpacity>
             </View>
         );
     }
--- src/features/Details/containers/Details.styles.js
+++ src/features/Details/containers/Details.styles.js
@@ -5,13 +5,33 @@ export const ITEM_WIDTH = 100;
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        paddingHorizontal: 15,
         backgroundColor: '#fafafa',
         paddingTop: (Platform.OS === 'ios') ? 20 : 0,
+        position: 'relative'
     },
     image: {
         width: '100%',
         height: '100%'
+    },
+    content: {
+        position: 'absolute',
+        width: '100%',
+        height: 140,
+        backgroundColor: 'rgba(255,255,255,0.5)',
+        bottom: 0,
+        padding: 15,
+    },
+    text: {
+        flex: 1,
+        marginBottom: 15,
+    },
+    backButton: {
+        position: 'absolute',
+        top: 15,
+        left: 15
+    },
+    backButtonText: {
+        color: 'blue'
     }
 });
 
